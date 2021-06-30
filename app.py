1c08a4a7eb593b1d21ee988c9dcad91a4d4634f4
Tushar Mehta <56684401+Tushar00728@users.noreply.github.com>
2021-07-01 02:13:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = prediction[0]
 
-    return render_template('index.html', prediction_text = 'Heart_disease: (1 = Yes, 2 = No) {}'.format(output))
+    return render_template('index.html', prediction_text = 'Heart_disease: (1 = Yes, 0 = No) {}'.format(output))
 
 if __name__== "__main__":
     app.run(debug= True)
